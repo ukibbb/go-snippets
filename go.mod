8cdf1060a28043bd51d0188bb322285125f53b1d
Lukasz Bulinski <lucas.bulinski@gmail.com>
2024-12-29 19:10:50 +0100

    sending files over ssh

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ukibbb/go-snippets
+
+go 1.22.2
+
+require (
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/gliderlabs/ssh v0.3.8 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+)
---
+++ ssh_http_tunel.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gliderlabs/ssh"
+)
+
+type Tunnel struct {
+	w      io.Writer
+	donech chan struct{}
+}
+
+var tunnels = map[int]chan Tunnel{}
+
+func main() {
+
+	go func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			idstr := r.URL.Query().Get("id")
+			id, _ := strconv.Atoi(idstr)
+			tunnel, ok := tunnels[id]
+
+			if !ok {
+				w.Write([]byte("tunnel does not exist."))
+				return
+			}
+			donech := make(chan struct{})
+			tunnel <- Tunnel{
+				w:      w,
+				donech: donech,
+			}
+			<-donech
+
+		})
+		log.Fatal(http.ListenAndServe(":3000", nil))
+	}()
+
+	ssh.Handle(func(s ssh.Session) {
+		id := rand.Intn(math.MaxInt)
+		tunnels[id] = make(chan Tunnel)
+
+		fmt.Println("tunnel id -> ", id)
+
+		tunnel := <-tunnels[id]
+
+		fmt.Println("Channel is ready.")
+
+		_, err := io.Copy(tunnel.w, s)
+		if err != nil {
+			log.Fatal(err)
+		}
+		close(tunnel.donech)
+
+		s.Write([]byte("We are done!"))
+	})
+
+	log.Fatal(ssh.ListenAndServe(":2222", nil))
+
+}
